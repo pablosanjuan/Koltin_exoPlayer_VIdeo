a91141772bc167452f7f3bb7594be9f692502c8b
pablo.sanjuan <psanjuan@sofi.org>
2020-07-19 18:28:15 -0500

    mp3 file implemented

--- app/src/main/java/com/pablosj/exoplayer/MainActivity.kt
+++ app/src/main/java/com/pablosj/exoplayer/MainActivity.kt
@@ -1,102 +1,105 @@
 package com.pablosj.exoplayer
 
+import android.annotation.SuppressLint
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.google.android.exoplayer2.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
-import com.google.android.exoplayer2.source.TrackGroupArray
-import com.google.android.exoplayer2.source.dash.DashMediaSource
-import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), Player.EventListener {
 
-    lateinit var simpleExoPlayer: SimpleExoPlayer
-    private var playBackPositionn = 0L
-    private val dashUrl = "http://rdmedia.bbc.co.uk/dash/ondemand/bbb/2/client_manifest-separate_init.mpd"
-    private val bandWidthMeter by lazy {
-        DefaultBandwidthMeter()
-    }
-    private val adaptiveTrackSelectionFactory by lazy {
-        AdaptiveTrackSelection.Factory(bandWidthMeter)
-    }
+class MainActivity : AppCompatActivity(){
+
+    var player: SimpleExoPlayer? = null
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition: Long = 0
+//    private var playBackPositionn = 0L
+//    private val dashUrl = "http://rdmedia.bbc.co.uk/dash/ondemand/bbb/2/client_manifest-separate_init.mpd"
+//    private val bandWidthMeter by lazy {
+//        DefaultBandwidthMeter()
+//    }
+//    private val adaptiveTrackSelectionFactory by lazy {
+//        AdaptiveTrackSelection.Factory(bandWidthMeter)
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    private fun initializePlayer(){
+        player = SimpleExoPlayer
+            .Builder(this)
+            .build()
+        playerView.player = player
+
+        val uri: Uri = Uri.parse("http://storage.googleapis.com/exoplayer-test-media-0/play.mp3")
+        val mediaSource: MediaSource = buildMediaSource(uri)
+        player!!.playWhenReady = playWhenReady
+        player!!.seekTo(currentWindow, playbackPosition)
+        player!!.prepare(mediaSource, false, false)
+    }
+
+    private fun buildMediaSource(uri: Uri): MediaSource {
+        val dataSourceFactory: DataSource.Factory =
+            DefaultDataSourceFactory(this, "exoplayer-codelab")
+        return ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(uri)
+    }
+
+    private fun releasePlayer() {
+        if (player != null) {
+            playWhenReady = player!!.playWhenReady
+            playbackPosition = player!!.currentPosition
+            currentWindow = player!!.currentWindowIndex
+            player!!.release()
+            player = null
+        }
+    }
+
     override fun onStart() {
         super.onStart()
-        initializeExoPLayer()
+        if (Util.SDK_INT >= 24) {
+            initializePlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT < 24 || player == null) {
+            initializePlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
     }
 
     override fun onStop() {
         super.onStop()
-        releaseExoPLayer()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
     }
 
-    private fun initializeExoPLayer(){
-        simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(
-            DefaultRenderersFactory(this),
-            DefaultTrackSelector(adaptiveTrackSelectionFactory),
-            DefaultLoadControl()
-        )
-        prepareExoPLayer()
-        ep_simple_exo_player.player = simpleExoPlayer
-        simpleExoPlayer.seekTo(playBackPositionn)
-        simpleExoPlayer.playWhenReady = true
-        simpleExoPlayer.addListener(this)
-    }
-    private fun releaseExoPLayer(){
-        playBackPositionn = simpleExoPlayer.currentPosition
-        simpleExoPlayer.release()
-    }
-
-    private fun buidMediaSource(uri: Uri): MediaSource {
-        val dataSourceFactory = DefaultHttpDataSourceFactory("ua", bandWidthMeter)
-        val dashChunckSourceFactory = DefaultDashChunkSource.Factory(dataSourceFactory)
-        return DashMediaSource(uri, dataSourceFactory, dashChunckSourceFactory, null, null)
-    }
-
-    private fun prepareExoPLayer(){
-        val uri = Uri.parse(dashUrl)
-        val mediaSource = buidMediaSource(uri)
-        simpleExoPlayer.prepare(mediaSource)
-    }
-
-    override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters) {
-    }
-
-    override fun onTracksChanged(trackGroups: TrackGroupArray, trackSelections: TrackSelectionArray) {
-    }
-
-    override fun onPlayerError(error: ExoPlaybackException) {
-    }
-
-    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
-        if (playbackState == Player.STATE_BUFFERING)
-            pb_progress_bar.visibility = View.VISIBLE
-        else if (playbackState == Player.STATE_READY)
-            pb_progress_bar.visibility == View.INVISIBLE
-    }
-
-    override fun onLoadingChanged(isLoading: Boolean) {
-    }
-
-    override fun onPositionDiscontinuity() {
-    }
-
-    override fun onRepeatModeChanged(repeatMode: Int) {
-    }
-
-    override fun onTimelineChanged(timeline: Timeline?, manifest: Any?) {
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        playerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
     }
 }
